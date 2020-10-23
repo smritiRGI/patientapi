f9402c8127802fc28733673fc6dbd35b1e3487fc
smritiRGI <smritirastogi33@gmail.com>
2020-10-23 15:29:06 +0530

    update added

--- src/pages/Update.js
+++ src/pages/Update.js
@@ -1,57 +1,62 @@
-import React,{Component} from 'react';
+import React, {Component} from 'react';
 import axios from 'axios';
-import './Update.css';
+import './register.css';
 
-class Update extends Component {
-     constructor(props){
+class Register extends Component{
+    constructor(props){
         super(props);
         this.state = {
-          id:"",
-          name:"",
-          dob:"",
-          address:"",
-          contact:"",
-          email:"",
-          diagnosis:"",
-          doctor:"",
-          appointment_date:"",
-          appointment_time:""
-      }
-     }
+            id:'',
+            name:'',
+            dob:'',
+            address:'',
+            contact:'',
+            email:'',
+            doctor:'',
+            diagnosis:'',
+            appointmentdate:'',
+            appointmenttime:''
+        }
+    }
 
-     handleSubmit = event =>{
-      event.preventDefault();
-      axios.get(`http://localhost:5000/api/Patient/GetId/${this.state.id}`)
-      .then(res => {
-          this.setState({
-            patient_name:this.state.name,
-            DOB:this.state.dob,
+    handleSubmit = (event) =>{
+        event.preventDefault();
+        axios.put('http://localhost:5000/api/Patient/put' , { 
+        id:this.state.id,
+        patient_name:this.state.name,
+        DOB:this.state.dob,
         address:this.state.address,
         contact:this.state.contact,
         email:this.state.email,
         diagnosis_reason:this.state.diagnosis,
         doctor_name:this.state.doctor,
-        appointment_date:this.state.appointment_date,
-        appointment_time:this.state.appointment_time
-            })
-            console.log(res.data);
-      })
-      .catch(error =>{
-      console.log(error);
-      })
-  }
+        appointment_date:this.state.appointmentdate,
+        appointment_time:this.state.appointmenttime}
+       )
+    .then(json => {
+        console.log(json);
+    })
+    .catch(error =>{
+        console.log(error);
+    })
+    }
 
-  handleChange = (event) => {
-    this.setState({[event.target.name]: event.target.value});
-    this.renderTable();
-  }
-
-  renderTable  = ()=>{
-      return(
-      <div>
-        <form>
+    handleChange =(event)=>{
+        this.setState({
+                [event.target.name]: event.target.value
+            });
+     }
+    
+  
+    render(){
+        return(
+        <div>
+        <form onSubmit={this.handleSubmit}>
             <h1>Update</h1>
-            <p>Please fill in this form to update your details.</p>
+            <div className="form-group">
+            <label>Patient ID</label>
+            <input type="text" className="form-control" name="id" value={this.state.id} onChange ={this.handleChange}/>
+            </div>
             <div className="form-group">
             <label>Patient Name</label>
             <input type="text" className="form-control" name="name" value={this.state.name} onChange ={this.handleChange}/>
@@ -66,7 +71,7 @@ class Update extends Component {
             </div>
             <div className="form-group">
             <label>Contact</label>
-            <input type="text" className="form-control" name="contact"value={this.state.contact}  onChange ={this.handleChange}/>
+            <input type="text" className="form-control" name="contact" value={this.state.contact} onChange ={this.handleChange}/>
             </div>
             <div className="form-group">
             <label>Email</label>
@@ -81,31 +86,17 @@ class Update extends Component {
             <input type="text" className="form-control" name="diagnosis" value={this.state.diagnosis} onChange={this.handleChange}/>
             </div>
             <div className="form-group">
-            <label>appointment_date</label>_
-            <input type="date" className="form-control" name="appointment_date" value={this.state.appointmentdate} onChange={this.handleChange}/>
+            <label>appointment_date</label>
+            <input type="date" className="form-control" name="appointmentdate" value={this.state.appointment_date} onChange={this.handleChange}/>
             </div>
             <div className="form-group">
             <label>appointment_time</label>
-            <input type="text" className="form-control" name="appointment_time" value={this.state.appointmenttime} onChange={this.handleChange}/>
+            <input type="text" className="form-control" name="appointmenttime" value={this.state.appointment_time} onChange={this.handleChange}/>
             </div>
             <button type="submit">Update</button>
         </form>
       </div>
-      )
-  }
-  
-    render(){
-      return(
-        <form onSubmit={this.handleSubmit}>
-        <div className="form-group">
-            <label>Patient ID</label>
-            <input type="text" className="form-control"  name="id" value={this.state.id} onChange ={this.handleChange}/>
-        </div>
-        <button type="submit">Search</button>
-        </form>
-      )
-    }
-  }
+)}
+}
 
-
-  export default Update;
+export default Register;
